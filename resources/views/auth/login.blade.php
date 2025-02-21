29fcbcd60497eb8489b8d3c123f2babc42182349
br3-mah <nyeleti.bremah@gmail.com>
2025-02-21 18:30:00 +0200

    Login & Registration UI Enhancement

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Site Title -->
     <title>Capex Financial - Sign In</title>
-    <link rel="shortcut icon" href="/api/placeholder/32/32" alt="Capex favicon">
+    <link rel="shortcut icon" href="public/app/img/fav.png" alt="Capex favicon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
